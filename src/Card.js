f4981ee2c465c80d97a81aa45d7f61945265206b
Adel <htwr86@gmail.com>
2019-06-05 00:01:05 +0300

    add CardList component

---
+++ src/Card.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Card = ({id, name, email}) => {
+  return (
+    <div className='bg-light-green dib br3 pa3 ma2 tc grow bw2 shadow-5'>
+      <img src={`https://robohash.org/tel${id}?200x200`} alt="im"/>
+      <div>
+        <h2>{name}</h2>
+        <p>{email}</p>
+      </div>
+    </div>
+  );
+}
+export default Card; 
